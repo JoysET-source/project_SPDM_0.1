65fc7a45d7a2fdee144c27526eab7f5cd867e164
JoysET-source <joys7919@gmail.com>
2025-02-21 16:26:04 +0200

    SPDM.0.1 under construction

--- models/ricetta_model.py
+++ models/ricetta_model.py
@@ -8,24 +8,24 @@ class Ricetta(db.Model):
     nome_ricetta = db.Column(db.String(100), unique=True, nullable=False)
     ingredienti = db.Column(db.String, nullable=False)
     kcal = db.Column(db.Integer, nullable=False)
-    image_url = db.Column(db.String(500), nullable=True)  # Aggiungi questo campo per memorizzare l'URL(il percorso) dell'immagine che sta su static
-    category = db.Column(db.String, nullable=False)
-    title = db.Column(db.String, nullable=False)
-    description = db.Column(db.String, nullable=False)
-    visibility = db.Column(db.Boolean, nullable=False)
-    servings = db.Column(db.Integer, nullable=False)
-    preparation_time = db.Column(db.Integer, nullable=False)
-    coocking_time = db.Column(db.Integer, nullable=False)
-    total_time = db.Column(db.Integer, nullable=False)
-    steps = db.Column(db.String, nullable=False)
-    difficulty_level = db.Column(db.String, nullable=False)
-    cusine_type = db.Column(db.String, nullable=False)
-    author = db.Column(db.String, nullable=False)
-    rating = db.Column(db.Integer, nullable=False)
-    tags = db.Column(db.String, nullable=False)
-    price = db.Column(db.Integer, nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=func.now()) # Assegna la data automatica alla creazione
-    updated_at = db.Column(db.DateTime, nullable=False, default=func.now(), onupdate=func.now()) # Aggiorna ogni volta che viene modificato
+    # image_url = db.Column(db.String(500), nullable=True)  # Aggiungi questo campo per memorizzare l'URL(il percorso) dell'immagine che sta su static
+    # category = db.Column(db.String, nullable=False)
+    # title = db.Column(db.String, nullable=False)
+    # description = db.Column(db.String, nullable=False)
+    # visibility = db.Column(db.Boolean, nullable=False)
+    # servings = db.Column(db.Integer, nullable=False)
+    # preparation_time = db.Column(db.Integer, nullable=False)
+    # coocking_time = db.Column(db.Integer, nullable=False)
+    # total_time = db.Column(db.Integer, nullable=False)
+    # steps = db.Column(db.String, nullable=False)
+    # difficulty_level = db.Column(db.String, nullable=False)
+    # cusine_type = db.Column(db.String, nullable=False)
+    # author = db.Column(db.String, nullable=False)
+    # rating = db.Column(db.Integer, nullable=False)
+    # tags = db.Column(db.String, nullable=False)
+    # price = db.Column(db.Integer, nullable=False)
+    # created_at = db.Column(db.DateTime, nullable=False, default=func.now()) # Assegna la data automatica alla creazione
+    # updated_at = db.Column(db.DateTime, nullable=False, default=func.now(), onupdate=func.now()) # Aggiorna ogni volta che viene modificato
 
 
 
--- routes/manage_recipe.py
+++ routes/manage_recipe.py
@@ -2,22 +2,30 @@ from flask import Blueprint, render_template
 
 dashboard_routes = Blueprint("dashboard_routes", __name__)
 
-@dashboard_routes.route("/admin_dashboard", methods=["GET", "POST"])
+@dashboard_routes.route("/admin_dashboard")
 def admin_dashboard():
     return render_template("dashboard/admin_dashboard.html")
 
 @dashboard_routes.route("/create_recipe", methods=["GET", "POST"])
 def create_recipe():
-    return render_template("dashboard/create_recipe.html")
+    return render_template("dashboard/ricette/create_recipe.html")
 
 @dashboard_routes.route("/read_recipe", methods=["GET"])
 def read_recipe():
-    return render_template("dashboard/read_recipe.html")
+    return render_template("dashboard/ricette/read_recipe.html")
 
 @dashboard_routes.route("/update_recipe", methods=["GET","POST"])
 def update_recipe():
-    return render_template("dashboard/update_recipe.html")
+    return render_template("dashboard/ricette/update_recipe.html")
 
 @dashboard_routes.route("/delete_recipe", methods=["GET", "POST"])
 def delete_recipe():
-    return render_template("dashboard/delete_recipe.html")
+    return render_template("dashboard/ricette/delete_recipe.html")
+
+@dashboard_routes.route("/footer")
+def footer():
+    return render_template("dashboard/footer.html")
+
+@dashboard_routes.route("/header")
+def header():
+    return render_template("dashboard/header.html")
