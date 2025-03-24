ae6ec1cb5e20feb0d73535db5dea1ace3ce25138
JoysET-source <joys7919@gmail.com>
2025-03-24 15:24:21 +0200

    interfaccia admin

--- static/dashboard/js/admin_dsh_script.js
+++ static/dashboard/js/admin_dsh_script.js
@@ -1,50 +1,35 @@
 
-// crea il menu a barre
+// controlla il menu a barre
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const menuToggle = document.querySelector('.menu-toggle');
+//     const nav = document.querySelector('nav');
+//
+// // gestisce la visibilità del menu trasformando NONE(nascosto) in BLOCK(visibile)
+//     menuToggle.addEventListener('click', function() {
+//         if (nav.style.display === "none" || nav.style.display === "") {
+//             nav.style.display = "block";  // Mostra il menu
+//         } else {
+//             nav.style.display = "none";  // Nascondi il menu
+//         }
+//     });
+// });
+
+
+// controlla il menu a barre
 document.addEventListener('DOMContentLoaded', function() {
     const menuToggle = document.querySelector('.menu-toggle');
-    const nav = document.querySelector('nav');
+    const navMenu = document.getElementById('menu');
 
-// gestisce la visibilità del menu trasformando NONE(nascosto) in BLOCK(visibile)
-    menuToggle.addEventListener('click', function() {        
-        if (nav.style.display === "none" || nav.style.display === "") {
-            nav.style.display = "block";  // Mostra il menu
-        } else {
-            nav.style.display = "none";  // Nascondi il menu
+    menuToggle.addEventListener('click', function(event) {
+        navMenu.classList.toggle('hidden');
+        event.stopPropagation(); // Impedisce la propagazione del click al documento
+    });
+
+    document.addEventListener('click', function(event) {
+        if (!navMenu.classList.contains('hidden') && !navMenu.contains(event.target) && !menuToggle.contains(event.target)) {
+            navMenu.classList.add('hidden');
         }
-    });
-});
-
-// ========================================== non utilizzato =========================================================
-
-// script per inizializzare editor di testo Quill
-document.addEventListener("DOMContentLoaded", function () {
-    // // Inizializzare Quill per Descrizione
-    // const quillDescrizione = new Quill('#editorDescrizione', {
-    //     theme: "snow"
-    // });
-
-    // Inizializzare Quill per Ingredienti
-    const quillIngredienti = new Quill('#editorIngredienti', {
-        theme: "snow"
-    });
-
-    // Inizializzare Quill per Steps
-    const quillSteps = new Quill('#editorSteps', {
-        theme: "snow"
-    });
-
-    // Mandare il contenuto del Quill al backend quando il form viene inviato
-    document.querySelector("form").addEventListener("submit", function (event) {
-        // Impedire che il form venga inviato prima di aggiornare i campi nascosti
-        event.preventDefault();
-
-        // Aggiornare i campi nascosti con il contenuto degli editor Quill
-        // document.querySelector("#hiddenDescrizione").value = quillDescrizione.root.innerHTML;
-        document.querySelector("#hiddenIngredienti").value = quillIngredienti.root.innerHTML;
-        document.querySelector("#hiddenSteps").value = quillSteps.root.innerHTML;
-
-        // Invia il form dopo aver aggiornato i campi nascosti
-        this.submit();
     });
 });
 
