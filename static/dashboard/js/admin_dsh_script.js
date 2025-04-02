9ba789f880fff069258bbcc9a3a90097a4799483
JoysET-source <joys7919@gmail.com>
2025-04-02 13:23:22 +0300

    interfaccia admin

--- static/dashboard/js/admin_dsh_script.js
+++ static/dashboard/js/admin_dsh_script.js
@@ -1,32 +1,14 @@
-
 // controlla il menu a barre
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     const menuToggle = document.querySelector('.menu-toggle');
-//     const nav = document.querySelector('nav');
-//
-// // gestisce la visibilità del menu trasformando NONE(nascosto) in BLOCK(visibile)
-//     menuToggle.addEventListener('click', function() {
-//         if (nav.style.display === "none" || nav.style.display === "") {
-//             nav.style.display = "block";  // Mostra il menu
-//         } else {
-//             nav.style.display = "none";  // Nascondi il menu
-//         }
-//     });
-// });
-
-
-// controlla il menu a barre
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const menuToggle = document.querySelector('.menu-toggle');
     const navMenu = document.getElementById('menu');
 
-    menuToggle.addEventListener('click', function(event) {
+    menuToggle.addEventListener('click', function (event) {
         navMenu.classList.toggle('hidden');
         event.stopPropagation(); // Impedisce la propagazione del click al documento
     });
 
-    document.addEventListener('click', function(event) {
+    document.addEventListener('click', function (event) {
         if (!navMenu.classList.contains('hidden') && !navMenu.contains(event.target) && !menuToggle.contains(event.target)) {
             navMenu.classList.add('hidden');
         }
@@ -34,4 +16,15 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+<!-- si occupa di  andare a capo in textarea ingredienti  -->
+document.querySelector("textarea[name='ingredienti']").addEventListener("blur", function () {
+    document.querySelector(".testo-ingredienti").textContent = this.value;
+});
+
+
+document.querySelector("textarea[name='steps']").addEventListener("blur", function () {
+    document.querySelector(".testo-steps").textContent = this.value;
+});
+
+<!-- ====================== -->
 
