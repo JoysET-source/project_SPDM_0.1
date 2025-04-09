ae252e085c4fd78b4fdaed0666ee9ad5767d9646
JoysET-source <joys7919@gmail.com>
2025-04-09 10:37:14 +0300

    modifiche

--- models/user_model.py
+++ models/user_model.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from sqlalchemy import func
 
 
 from flask_login import UserMixin
@@ -13,6 +13,6 @@ class User(db.Model, UserMixin):
     # email = db.Column(db.String, nullable=True)
     username = db.Column(db.String(20), unique=True, nullable=True)
     password = db.Column(db.String(255), nullable=True)
-    # prima_visita = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
-    # ultima_visita = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    prima_visita = db.Column(db.DateTime, nullable=True, default=func.now())
+    ultima_visita = db.Column(db.DateTime, nullable=True, default=func.now(), onupdate=func.now())
     access_logs = db.relationship('AccessLog', back_populates='user')
