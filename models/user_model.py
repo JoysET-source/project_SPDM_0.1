c4278489c40884cadbb391cc904e8f9029238522
JoysET-source <joys7919@gmail.com>
2025-06-03 14:43:28 +0300

    modello per la class User

--- models/user_model.py
+++ models/user_model.py
@@ -9,7 +9,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     # admin = db.Column(db.String, unique=True, nullable=True)
     # email = db.Column(db.String, nullable=True)
-    username = db.Column(db.String(20), unique=True, nullable=True)
+    username = db.Column(db.String(255), unique=True, nullable=True)
     password = db.Column(db.String(255), nullable=True)
     prima_visita = db.Column(db.DateTime, nullable=True, default=func.now())
     ultima_visita = db.Column(db.DateTime, nullable=True, default=func.now(), onupdate=func.now())
